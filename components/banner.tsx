1ff1c5a8d7e4222e265b009b89f3605b7714c3d0
Joshua Singh <joshua.d.singh@outlook.com>
2022-06-10 16:59:50 +0200

    feat(ZAR-021): Adds blur placeholder to banners

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Banner.module.css';
+import { rgbDataURL } from '../utils/helpers';
 
 type BannerProps = {
     bgImage: string,
@@ -10,7 +11,7 @@ type BannerProps = {
 
 const Banner: FunctionComponent<BannerProps> = ({ bgImage, children, className }) => (
     <div className={styles.banner}>
-        <Image priority className={styles.banner_bgImage} layout='fill' objectPosition='50% 75%' objectFit='cover' src={bgImage} alt='hero bg' />
+        <Image placeholder="blur" blurDataURL={rgbDataURL(225, 225, 225)} priority className={styles.banner_bgImage} layout='fill' objectPosition='50% 75%' objectFit='cover' src={bgImage} alt='hero bg' />
         <div className={`container ${styles.banner_inner} ${className ? className : ''}`}>
             {children}
         </div>
